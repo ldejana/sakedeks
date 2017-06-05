2104f4a6cbdc04eb8d301ca9260436831aff788c
sakesake <srpvlvc@gmail.com>
2017-06-05 12:00:12 +0200

    Update

---
+++ BookingApp/BookingApp/Models/Accommodation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookingApp.Models
+{
+    public class Accommodation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        [StringLength(1024)]
+        public string Description { get; set; }
+
+        [StringLength(256)]
+        public string Address { get; set; }
+
+        [Range(0, 5)]
+        public decimal AverageGrade { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public bool Approved { get; set; }
+    }
+}
---
+++ BookingApp/BookingApp/Models/BAContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookingApp.Models
+{
+    public class BAContext: IdentityDbContext<BAIdentityUser>
+    {   
+        public virtual DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Accommodation> Accommodations { get; set; }
+
+        public BAContext() : base("name=BADB")
+        {            
+        }
+
+        public static BAContext Create()
+        {
+            return new BAContext();
+        }
+    }
+}
