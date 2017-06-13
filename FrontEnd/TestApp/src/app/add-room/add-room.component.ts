084bca5712a62fbeb6f10ade407e2d5a227ed3a0
ldejana <djnlzvc@gmail.com>
2017-06-13 15:54:44 +0200

    Bug fix: room creating compiler error

--- FrontEnd/TestApp/src/app/add-room/add-room.component.ts
+++ FrontEnd/TestApp/src/app/add-room/add-room.component.ts
@@ -1,27 +1,25 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { AddRoomService } from "app/add-room/add-room.service";
-import { Router } from "@angular/router/src";
 import { Room } from "app/room/room.model";
 
 @Component({
-  selector: 'app-add-room',
+  selector: 'add-room',
   templateUrl: './add-room.component.html',
   styleUrls: ['./add-room.component.css'],
   providers: [ AddRoomService ]
 })
 export class AddRoomComponent implements OnInit {
 
-   
     Message: string = "";
 
     RoomNumber: number;
     BedCount: number;
     Description: string;
     PricePerNight: number;
-    @Input() AccommodationId: number;
+    @Input() AccommodationId: number = 1007;
     @Output() onRoomAdded: EventEmitter<any>;
     
-    constructor(private addRoomService: AddRoomService, private router: Router) {
+    constructor(private addRoomService: AddRoomService) {
       this.onRoomAdded = new EventEmitter();
     }
 
