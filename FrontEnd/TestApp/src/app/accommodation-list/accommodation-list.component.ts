82583f81b078715fa571c90898b33d5254c567c6
sakesake <srpvlvc@gmail.com>
2017-06-15 13:49:26 +0200

    Paging

--- FrontEnd/TestApp/src/app/accommodation-list/accommodation-list.component.ts
+++ FrontEnd/TestApp/src/app/accommodation-list/accommodation-list.component.ts
@@ -3,12 +3,13 @@ import {Accommodation} from '../accommodation/accommodation.model';
 import {AccommodationListService} from './accommodation-list.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Origins } from '../enumerations/origins.model';
+import { PagingService } from '../services/paging.service';
 
 @Component({
   selector: 'app-accommodation-list',
   templateUrl: './accommodation-list.component.html',
   styleUrls: ['./accommodation-list.component.css'],
-  providers: [AccommodationListService]
+  providers: [AccommodationListService, PagingService]
 })
 export class AccommodationListComponent implements OnInit {
 
@@ -17,8 +18,10 @@ export class AccommodationListComponent implements OnInit {
   origin: Origins;
   Accommodations: Accommodation[];
   placeName: string;
+  counter: Array<number>;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute, private accListService: AccommodationListService) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private accListService: AccommodationListService,
+      private pagingService: PagingService) {
     activatedRoute.params.subscribe(params => {this.Id = params["Id"]; 
                                                this.path = params["Name"]; 
                                                this.origin = params["Origin"];
@@ -36,12 +39,23 @@ export class AccommodationListComponent implements OnInit {
         case 'Region':
             this.accListService.getByRegionId(this.Id).subscribe(x => this.Accommodations = x.json()); break;
         case 'Place': {
-            this.accListService.getByPlaceId(this.Id).subscribe(x => this.Accommodations = x.json());
+            this.accListService.getByPlaceId(this.Id, 1, PagingService.PageSize).subscribe(x => {
+              this.Accommodations = (x.json()).value;
+              this.pagingService.initPagingService(x);
+
+               this.counter = new Array(PagingService.PageNumber);
+            });
             this.path = this.path + " >> " + this.placeName;
             break;
         }
         default: break;
       }
+  }
+
+  changePage(pageNumber: number){
+    this.accListService.getByPlaceId(this.Id, pageNumber, PagingService.PageSize).subscribe(x => {
+              this.Accommodations = (x.json()).value; 
+            });
   }
 
 }
